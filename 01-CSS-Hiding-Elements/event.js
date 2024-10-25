180929702b132248ad23527d4968ee0b910c0b25
rita7811 <92129512+rita7811@users.noreply.github.com>
2024-10-25 18:06:14 -0400

    update 01-CSS-Hiding-Elements

--- 01-CSS-Hiding-Elements/event.js
+++ 01-CSS-Hiding-Elements/event.js
@@ -1,23 +1,31 @@
-const displayEle = document.getElementById("display-none");
-const displayBtnEle = document.getElementById("child-display");
-
-const opacityEle = document.getElementById("opacity-0");
-const opacityBtnEle = document.getElementById("child-opacity");
-
-const visibilityEle = document.getElementById("visibility-hidden");
-const visibilityBtnEle = document.getElementById("child-visibility");
+let x = 0, y = 0, z = 0
 
 const originalEle = document.getElementById("original-box");
 const originalBtnEle = document.getElementById("child-original");
 
-const clickedEle = document.getElementById("clicked");
+const displayEle = document.getElementById("display-none");
+const displayBtnEle = document.getElementById("child-display");
 
-function clickEvent() {
-    clickedEle.style.color = "black";
-    clickedEle.innerHTML += "clicked ";
+const visibilityEle = document.getElementById("visibility-hidden");
+const visibilityBtnEle = document.getElementById("child-visibility");
+
+const opacityEle = document.getElementById("opacity-0");
+const opacityBtnEle = document.getElementById("child-opacity");
+
+function clickEvent(e) {
+    if (e.target.id === 'child-original') {
+        e.target.innerHTML = `Child Element Clicked ${x += 1} `;
+    }
+    if (e.target.id === 'child-visibility') {
+        e.target.innerHTML = `Child Element Clicked ${y += 1} `;
+    }
+    if (e.target.id === 'child-opacity') {
+        e.target.innerHTML = `Child Element Clicked ${z += 1} `;
+    }
 }
 
-// Box
+
+// Original Box
 originalEle.addEventListener("mouseover", function () {
     originalEle.style.backgroundColor = "red";
 })
@@ -30,6 +38,7 @@ originalBtnEle.addEventListener("click", clickEvent);
 // Box 1 => display: none
 displayEle.addEventListener("mouseover", function () {
     displayEle.style.display = "block";
+    displayEle.style.backgroundColor = "red";
 })
 displayEle.addEventListener("mouseout",function () {
     displayEle.style.display = "none";
@@ -37,26 +46,24 @@ displayEle.addEventListener("mouseout",function () {
 displayBtnEle.addEventListener("click", clickEvent);
 
 
-// Box 2 => opacity: 0
-opacityEle.addEventListener("click", function () {
-    clickedEle.style.color = "red";
-    clickedEle.innerHTML += "opacity ";
-})
-opacityBtnEle.addEventListener("mouseover", function () {
-    opacityEle.style.opacity = "1";
-    clickedEle.style.color = "blue";
-    clickedEle.innerHTML += "hover ";
-})
-opacityBtnEle.addEventListener("mouseout", function () {
-    opacityEle.style.opacity = "0";
-})
-
-
-// Box 3 => visibility: hidden
-visibilityEle.addEventListener("click", function () {
+// Box 2 => visibility: hidden
+visibilityEle.addEventListener("mouseover", function () {
     visibilityEle.style.visibility = "visible";
+    visibilityEle.style.backgroundColor = "red";
 })
 visibilityEle.addEventListener("mouseout",function () {
-    visibilityEle.style.backgroundColor = "hidden";
+    visibilityEle.style.visibility = "hidden";
 })
 visibilityBtnEle.addEventListener("click", clickEvent);
+
+
+// Box 3 => opacity: 0
+opacityEle.addEventListener("mouseover", function () {
+    opacityEle.style.opacity = "1";
+    opacityEle.style.backgroundColor = "red";
+})
+opacityEle.addEventListener("mouseout",function () {
+    opacityEle.style.opacity = "0";
+})
+opacityBtnEle.addEventListener("click", clickEvent)
+
